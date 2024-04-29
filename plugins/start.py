32c57aa4d72d15a067a7fcb85843a76a7357f20c
actanimemm <96991770+actanimemm@users.noreply.github.com>
2024-04-29 10:30:51 +0630

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton(" About Me", callback_data = "about"),
+                    InlineKeyboardButton(" Close", callback_data = "close")
                 ]
             ]
         )
@@ -131,7 +131,7 @@ async def not_joined(client: Client, message: Message):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text = 'Try Again',
+                    text = 'Try Again ကိုနှိပ်ပါ',
                     url = f"https://t.me/{client.username}?start={message.command[1]}"
                 )
             ]
